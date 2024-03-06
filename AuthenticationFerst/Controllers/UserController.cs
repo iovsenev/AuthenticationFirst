1c392f443f774028a7dda15840b347cd0d238bd7
Ilya Ovsenev <ovsenev.ilya@yandex.ru>
2024-03-06 10:18:42 +0300

    Add project files.

---
+++ AuthenticationFerst/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationFirst.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private ILogger _logger;
+
+        public UserController(ILogger loger)
+        {
+            _logger = loger;
+
+            _logger.WriteError("Error message!");
+            _logger.WriteEvent("Event message!");
+        }
+
+        public User GetUser()
+        {
+            return new User()
+            {
+                LastName = "Ivanov",
+                Name = "Ivan",
+                Email = "asdfa@adsfa.ru",
+                Id = Guid.NewGuid(),
+                Login = "ivanov",
+                Password = "qwer1234"
+            };
+
+        }
+    }
+}
---
+++ AuthenticationFerst/User.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthenticationFirst
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Login {  get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
